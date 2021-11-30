34ff3c9f40027684c9ccff3792b9fbd1f7bda98e
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-30 18:11:54 -0500

    add unit test, add get request to homepage, add post request for adding todo

--- routes/client.js
+++ routes/client.js
@@ -6,19 +6,44 @@
  */
 
 const express = require('express');
+const { addItem,allMovies} = require('../databaseHelper/databaseHelper');
 const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("index");
   });
-  router.get("/:user_id",(req, res) => {
-    const user_id = req.params.user_id
-    res.render("partials/_userpage");
-  });
   router.post("/add/:user_id",(req, res) => {
-    // res.send("/");
+      const item = req.body.item
+      const user = 1
+      addItem(item,user,db)
+      .then((result) => {
+        if(!item){
+          res.send({error: "no item in body"})
+          return;
+        }
+        // console.log(result.rows[0])
+        // res.send(result.rows[0]);
+        res.redirect (`/${user}`)
+      })
+      .catch((e) => res.send(e));
+
   });
+  router.get("/:user_id",(req, res) => {
+      allMovies(db)
+      .then((movies) => {
+      // console.log("movies",movies)
+        const templateVars = {user_id:1, movies}
+        res.render("partials/_userpage", templateVars);
+    });
+  });
+  
+
+
+
+
+
+
 
 return router;
 
