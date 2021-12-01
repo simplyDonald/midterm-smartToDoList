3020abc23460dba32fd0c8b45abe3b4c47966527
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-30 22:37:26 -0500

    add function that categorizes items

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,20 +1,63 @@
-// Client facing scripts here
-// $(document).ready(function () {
+const matchKeyWords= {
+  shopping:{
+    keyWords: ["buy", "get", "purchase"],
+    categoryId: 104
+  },
+  films: {
+    keyWords: ["watch", "see", "Netflix"],
+    categoryId : 101
+  },
+  books: {
+    keyWords: ["read", "study"],
+    categoryId : 103
+  },
+  restaurants:{
+    keyWords: ["eat", "drink", "binge"],
+    categoryId : 102
+  }
+    }
+
+const categorizeItem = function(item,matchKeyWords){
+  let category_id;
+  let firstWord = item.toLowerCase().split(" ");
+  console.log(firstWord[0])
+  for (let key in matchKeyWords){
+    if (matchKeyWords[key].keyWords.includes(firstWord[0])){
+      console.log("matched", matchKeyWords[key].keyWords)
+      category_id = matchKeyWords[key].categoryId
+      break;
+    }
+  }
+  return category_id;
+}
+
+
+
+
+
+// // $(function () {
+//   $("#add-item").on("click", function(evt){
+//     evt.preventDefault();
+//     categorizeItem($("#input-item"),matchKeyWords);
+//     const category_id = categorizeItem($("#input-item"),matchKeyWords);
+//     $("#add-item")
+//     $("#add-item").val("");
+
+
+
+
+
+
+
+//   })
+
+
+
+// })
+
 //     function loadListItems(){
 //       $.ajax({
 //         type: "GET",
 //         url: ""
 //       })
 //     }
-
-
-
-//   // function loadTweets() {
-//   //   $.ajax({
-//   //     type: "GET",
-//   //     url: "/tweets",
-//   //     dataType: "json"
-//   //   }).then((res) => {
-//   //     renderTweets(res);
-//   //   })
-//   });
--- routes/todo.js
+++ routes/todo.js
@@ -18,12 +18,12 @@ module.exports = (db) => {
   })
 
  //user adds item to todolist
- router.post("/add/:user_id", (req,res) => {
+ router.post("/:user_id", (req,res) => {
   const user= 1
   const item = req.body.name
-  databaseHelper.addItem({item,user},db)
+  databaseHelper.addItem(item,user,db)
     .then((newItem) => {
-      console.log(newItem.rows[0])
+      console.log("Database newItem",newItem.rows[0])
       if(!newItem){
         res.send({error: "no item in body"})
         return;
@@ -35,9 +35,9 @@ module.exports = (db) => {
 //user can edit item in list
 router.post("/edit/:item_id", (req,res) => {
     const name = req.body.name
-    console.log({name})
+    // console.log({name})
     const item = req.params.item_id
-    console.log("id", req.params.item_id)
+    // console.log("id", req.params.item_id)
   databaseHelper.editItem(item,name,db)
     .then((res) => {
       if(!item){
