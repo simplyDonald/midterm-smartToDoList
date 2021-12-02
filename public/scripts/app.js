a03f1397ba86f8e2444a640dac724a297a9ecbb2
Linh Hoang <ellehoang@hotmail.com>
2021-12-02 16:48:20 -0500

    combo of category functionality, user profile functionality, app styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -46,30 +46,129 @@
 // }
 // }
 
+
+
+
+function addEditedItem(newItem){
+
+  const newTask =
+  `<div class="d-flex align-items-center">
+    <input class="form-input-shopping d-none" type="input" />
+    <span class= "item-text" data-id="<%= ${newItem}%>">${newItem} </span>
+  </div>
+  <div class="btn-group btn-group-toggle" data-toggle="button">
+
+    <button class="btn btn-submit save-button d-none">
+      <img src="https://img.icons8.com/emoji/48/000000/check-mark-emoji.png"/>
+    </button>
+
+    <button class="btn btn-submit edit-button">
+    <img src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-edit-interface-kiranshastry-lineal-kiranshastry.png" style="width:auto;height:25px"/>
+    </button>
+
+    <button id="delete-button" class="btn btn-submit">
+      <img src="https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/000000/external-delete-multimedia-kiranshastry-lineal-kiranshastry.png" style="width:auto;height:25px"/>
+    </button>
+  </div>`
+
+  return newTask;
+
+}
+
+
+
 $(document).ready(function () {
+//    $('.edit-button').on('click', function (evt){
+//       evt.preventDefault();
+//       $(this).addClass("d-none");
+//       $(".form-input-shopping").addClass("d-block");
+//     $('.form-input-shopping').change(function(){
+//     const input = $(this).val();
+//     $('.list-group-item').append('<li>'+ input + '</li>');
+//     $(this).val('');
+//     $(".form-input-shopping").addClass("d-none");
+//        })
+// })
+    $('.card-body').on('click', '#delete-button', function (evt){
+      console.log("clicked delete button!")
+      $(this).closest('form').submit()
+    });
+    $('.card-body').on('click', '.edit-button', function (evt){
+      console.log("clicked edit button!")
+      const inputForm =  $(this).closest(".list-group-item").find('.form-input-shopping')
+      inputForm.show();
+      const oldItem =$(this).closest(".list-group-item").find('.item-text')
+      oldItem.hide();
+
+      $(this).siblings('.save-button').show();
+    });
+
+    //     // const ul = li.parentNode
+    //     // const id= $("#delete-button");
+    //     // if(button.id === id){
+    //     //   ul.removeChild(li)
+    //     // }
 
 
+//     // })
+// });
 
-
-
-  
 });
-
-
-
-
-
-
-
-// function loadDbItems(){
-  //  $.ajax({
-  //    type: "GET",
-  //    url: "/:user_id",
+  //save button .on click
+  //data-id attribute
+  //  function editedItem(){$.ajax({
+  //    type: "POST",
+  //    url: "api/todo/edit/:item_id",
   //    dataType: "json"
-  //  }).then((res) => {
-  //     renderListItems(res)
+  //  }).then((res)=> {
+
   //  })
-  //  console.log(loadDbItems())
   // }
+  // $('.edit-button').on('click', function (evt){
+  //   evt.preventDefault();
+  //   $(this).addClass("d-none");
+  //     const btn= $(this)
+  //     const targetingItem = btn.parent().prev();
+  //     const btn2 = btn.parent();
+
+  //   // targeting the save button
+  //     const saveBtn = btn2.children(".save-button");
+  //     saveBtn.addClass('d-block').removeClass('d-none');
+
+  //     const textElement = targetingItem.children(".item-text");
+  //     const formInput = targetingItem.children(".form-input-shopping");
+  //   formInput.addClass('d-block').removeClass('d-none');
+  //   console.log(textElement);
+  //   textElement.addClass("d-none");
   // })
-  console.log("working!")
+  // $('.save-button').on('click', function (evt){
+  //   evt.preventDefault();
+  //   const shoppingItem = $(".form-input-shopping").val();
+  //   $(this).parent().prev().children('.item-text').text(shoppingItem);
+  //   console.log("shopping item======>", shoppingItem);
+
+  //   $(this).parent().parent().empty();
+  //   const newItem = $(".form-input-shopping").val();
+  //   console.log("this", this)
+  //   const $dataId = $(this).data("id");
+
+  //   // const newItem = $(".form-input").val();
+  //   console.log("newItem------->",textElement);
+  //   console.log(`Test function------>`,addEditedItem(shoppingItem));
+
+  //   $(this).parent().parent().html(addEditedItem(shoppingItem));
+
+  //   console.log(newItem)
+  //   $(".item-text").text(newItem);
+  //   $('.form-input-shopping').addClass('d-none').removeClass('d-block');
+  //   $('.item.text').addClass('d-block').removeClass('d-none');
+
+  //   $('.save-button').addClass('d-none').removeClass('d-block');
+  //   $('.edit-button').addClass('d-block').removeClass('d-none');
+
+
+
+
+
+
+
--- routes/client.js
+++ routes/client.js
@@ -7,7 +7,7 @@
 
 const { promiseImpl } = require('ejs');
 const express = require('express');
-const { addItem, getDbItems} = require('../databaseHelper/databaseHelper');
+const { addItem, getDbItems, deleteItem,getUserWithId} = require('../databaseHelper/databaseHelper');
 const router  = express.Router();
 
 
@@ -20,9 +20,9 @@ module.exports = (db) => {
 
   router.get("/:user_id",(req, res) => {
 
-    Promise.all([getDbItems(db,101), getDbItems (db,102), getDbItems(db,103), getDbItems(db,104), getDbItems(db,105)])
+    Promise.all([getDbItems(db,101), getDbItems (db,102), getDbItems(db,103), getDbItems(db,104), getDbItems(db,105),getUserWithId(1,db)])
       .then((result)=> {
-        const templateVars = {user_id:1, movies:result[0], restaurants:result[1], books: result[2], shopping: result[3], others: result[4]};
+        const templateVars = {user_id:1, movies:result[0], restaurants:result[1], books: result[2], shopping: result[3], others: result[4], user: result[5]};
         res.render("partials/_userpage", templateVars);
   })
       .catch(e => e.message);
@@ -50,6 +50,18 @@ module.exports = (db) => {
   });
 
 
+  // router.post("/api/todo/delete/:item_id", (req,res) =>{
+  //   const item = req.params.item_id
+  //   const user = 1
+  //    console.log("we are here!")
+  //   deleteItem(item,db)
+  //   .then((res) =>{
+  //     console.log(res.rows)
+  //     return res.redirect(`/${user}`);
+  //   })
+  //   .catch((e) => res.send(e));
+
+  // })
 
 
 
