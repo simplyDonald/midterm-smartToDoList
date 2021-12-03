f74240f2490e90900eb5da8a6d078ff86265944a
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-12-03 08:24:08 -0500

    add change to email form field

--- routes/client.js
+++ routes/client.js
@@ -15,8 +15,7 @@ const router  = express.Router();
 module.exports = (db) => {
 
   router.get("/", (req, res) => {
-    const templateVars = {user:''}
-    res.render("index",templateVars);
+    res.render("index");
   });
 
   router.get("/:user_id",(req, res) => {
@@ -24,10 +23,9 @@ module.exports = (db) => {
     Promise.all([getDbItems(db,101), getDbItems (db,102), getDbItems(db,103), getDbItems(db,104), getDbItems(db,105),getUserWithId(1,db)])
       .then((result)=> {
         const templateVars = {user_id:1, movies:result[0], restaurants:result[1], books: result[2], shopping: result[3], other: result[4], user: result[5]};
-        // console.log("movies------", movies)
         res.render("partials/_userpage", templateVars);
   })
-      //.catch(e => e.message);
+      .catch(e => e.message);
 
   })
 
