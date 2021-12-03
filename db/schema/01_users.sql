72331d33a490e5bfe4d0842969d82d81abca6170
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-12-03 00:35:32 -0500

    update branch

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users(
   id SERIAL PRIMARY KEY NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  birth_date DATE NOT NULL
+  email VARCHAR(255) ,
+  password VARCHAR(255),
+  first_name VARCHAR(255) ,
+  last_name VARCHAR(255) ,
+  birth_date DATE 
 );
