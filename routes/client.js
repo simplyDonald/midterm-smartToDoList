bd9a5b83798b24e4442d1ae6a24903ac358f8e38
simplyDonald <donaldabuah@gmail.com>
2022-02-02 03:42:53 +0000

    fix navbar render to show register button when no user

--- routes/client.js
+++ routes/client.js
@@ -15,7 +15,7 @@ const router  = express.Router();
 module.exports = (db) => {
 
   router.get("/", (req, res) => {
-    const templateVars = {user:{}};
+    const templateVars = {user:""};
     res.render("index", templateVars);
   });
 
